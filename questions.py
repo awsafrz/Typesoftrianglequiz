a285f567dd1a63fff764ab1fd0e4a1db966c4e52
awsaf :0 <139934971+awsafrz@users.noreply.github.com>
2024-05-25 13:21:06 +1000

    Logic error fixed and updated text to speech

---
+++ questions.py
@@ -0,0 +1,46 @@
+quiz_data = [
+    {
+        "question": "What type of triangle has three equal sides?",
+        "choices": ["A. Isosceles", "B. Equilateral", "C. Scalene"],
+        "answer": "B. Equilateral" 
+    },
+    {
+        "question": "What type of triangle has two equal sides?",
+        "choices": ["A. Isosceles", "B. Equilateral", "C. Scalene"],
+        "answer": "A. Isosceles"
+    },
+    {
+        "question": "What type of triangle has NO equal sides?",
+        "choices": ["A. Isosceles", "B. Equilateral", "C. Scalene"],
+        "answer": "C. Scalene"
+    },
+    {
+        "question": "What type of triangle has angles less than 90 degrees?",
+        "choices": ["A. Acute", "B. Obtuse", "C. Right angled"],
+        "answer": "A. Acute"
+    },
+    {
+        "question": "What type of triangle has one angle equal to 90 degrees?",
+        "choices": ["A. Acute", "B. Obtuse", "C. Right angled"],
+        "answer": "C. Right angled"
+    },
+    {
+        "question": "What type of triangle has one angle greater than 90 degrees?",
+        "choices": ["A. Acute", "B. Obtuse", "C. Right angled"],
+        "answer": "B. Obtuse"
+    },
+    {
+        "question": "What is the sum of the angles inside triangles?",
+        "choices": ["A. 180 degrees", "B. 270 degrees", "C. 360 degrees"],
+        "answer": "A. 180 degrees"
+    },
+    {
+        "question": "How many sides does a triangle have?",
+        "choices": ["A. 1", "B. 2", "C. 3"],
+        "answer": "C. 3"
+    },
+    {
+        "question": "How many angles are there inside a triangle?",
+        "choices": ["A. 3", "B. 4", "C. 5"],
+        "answer": ["A. 3"]
+    }
